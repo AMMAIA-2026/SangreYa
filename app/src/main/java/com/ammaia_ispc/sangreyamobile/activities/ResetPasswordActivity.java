640be72d907c0600d7f34659d9a044481753570d
Irina Pirles <iripirles@gmail.com>
2026-09-11 21:23:27 -0300

    fix: se agregan validaciones y botones de navegación

--- app/src/main/java/com/ammaia_ispc/sangreyamobile/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/ammaia_ispc/sangreyamobile/activities/ResetPasswordActivity.java
@@ -1,4 +1,4 @@
-package com.ammaia_ispc.sangreyamobile;
+package com.ammaia_ispc.sangreyamobile.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.ammaia_ispc.sangreyamobile.R;
 
 import java.util.regex.Pattern;
 
