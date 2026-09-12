10b76228bea8a1a3d5b0cc696bd853e48007fb49
Abigail Picone <abigailpicone.ispc@gmail.com>
2026-09-12 09:00:44 -0300

    feat: se agrega botón Volver  y botón Contactanos en sección Sobre Nosotros

--- app/src/main/java/com/ammaia_ispc/sangreyamobile/activities/AboutUsActivity.java
+++ app/src/main/java/com/ammaia_ispc/sangreyamobile/activities/AboutUsActivity.java
@@ -2,9 +2,12 @@ package com.ammaia_ispc.sangreyamobile.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.ammaia_ispc.sangreyamobile.R;
+import com.ammaia_ispc.sangreyamobile.helpers.NavigationHelper;
+
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        NavigationHelper.configureBackButton(this, R.id.btnBack);
+
+        findViewById(R.id.tvContactanos).setOnClickListener(v ->
+                startActivity(new Intent(this, ContactActivity.class)));
     }
 }
